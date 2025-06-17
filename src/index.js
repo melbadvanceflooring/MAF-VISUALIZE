6523cd366def4d9f47162f410beae80b8345ce80
melbadvanceflooring <melbadvanceflooring@gmail.com>
2025-06-17 13:12:27 +1000

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MAFVisual from "./MAFVisual";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MAFVisual />);
